cc4299001c9bc29a70f16be78b29a440a6bf8681
Gene Kloss <gene.e.kloss@erdc.dren.mil>
2023-09-21 19:14:55 -0600

    compare page is maybe 70% done   need to add:   - a plotly graph     - change the graph based on a dropdown full of variables   - dropdown to let the user change the background raster being accessed for each project   - toggle to let each map's camera be independent   what was done:   - got a 2 panel 3d cesium mapview going, with each camera affecting the other   - got the geoserver wms data coming in for each project layer in the app

--- tethysapp/green/controllers/compare_map_view.py
+++ tethysapp/green/controllers/compare_map_view.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from tethys_sdk.permissions import login_required
 from tethys_sdk.gizmos import CesiumMapView, MVLayer
@@ -11,15 +13,19 @@ def compare(request):
     """
     cesium_ion_token = app.get_custom_setting('cesium_api_key')
 
-    watershed = MVLayer(
-        source='ImageWMS',
-        legend_title='Park City',
-        options={
-            'url': 'http://localhost:8181/geoserver/wms',
-            'params': {'LAYERS': 'gssha:gssha_model'},
-            'serverType': 'geoserver'
-        },
-    )
+
+    # Hacky Cesium stuff
+    # to use within the javascript to load the Cesium library
+
+    # watershed = MVLayer(
+    #     source='ImageWMS',
+    #     legend_title='Park City',
+    #     options={
+    #         'url': 'http://localhost:8181/geoserver/wms',
+    #         'params': {'LAYERS': 'gssha:gssha_model'},
+    #         'serverType': 'geoserver'
+    #     },
+    # )
 
     terrain_provider = {
         'Cesium.createWorldTerrain': {
@@ -30,13 +36,51 @@ def compare(request):
 
     cesium_map_view = CesiumMapView(
         cesium_ion_token=cesium_ion_token,
-        layers=[watershed],
+        layers=[],
         terrain={'terrainProvider': terrain_provider},
     )
+
+    # end of hacky cesium stuff
+    
+    from tethysext.atcore.models.app_users import AppUser
+    from tethysext.atcore.services.model_database import ModelDatabase
+    from tethysapp.green.models import GsshaModel
+
+    gs_engine = app.get_spatial_dataset_service(
+                    name=app.GEOSERVER_NAME,
+                    as_engine=True
+                )
+    endpoint = gs_engine.endpoint.replace('/rest/', '')
+    
+
+    session = app.get_persistent_store_database(app.DATABASE_NAME, as_sessionmaker=True)()
+
+    dropdownList = []
+
+    try:
+        request_app_user = AppUser.get_app_user_from_request(request, session)
+        
+        gssha_models = request_app_user.get_resources(
+            session=session,
+            request=request,
+            of_type=GsshaModel,
+        )
+
+        for model in gssha_models:
+            print(model.get_attribute('database_id'))
+            db_id = model.get_attribute('database_id').replace("_", "-")
+            dropdownList.append([model.name, db_id])
+
+    finally:
+        session.close()
+
+    
 
     context = {
         'cesium_map_view': cesium_map_view,
         'cesium_ion_token': cesium_ion_token,
+        'ddl': dropdownList,
+        'gs_endpoint': endpoint,
     }
 
     return render(request, 'green/compare.html', context)
--- tethysapp/green/public/js/compare.js
+++ tethysapp/green/public/js/compare.js
@@ -2,16 +2,18 @@ $(function() {
   // We want our two views to be synced across time, so we create
   // a shared clock object that both views share
   var token = document.getElementById('cesium_ion_token').innerText;
-  console.log(document.getElementById('cesium_ion_token'));
-  console.log(token);
+  var gs_endpoint = document.getElementById('gs_endpoint').innerText;
   Cesium.Ion.defaultAccessToken = token;
-  // document.getElementById('cesium_ion_token').remove();
+  document.getElementById('cesium_ion_token').remove();
+
+  console.log(gs_endpoint);
 
   const clockViewModel = new Cesium.ClockViewModel();
   const options3D = {
     fullscreenButton: false,
     sceneModePicker: false,
     clockViewModel: clockViewModel,
+    terrainProvider : Cesium.createWorldTerrain(),
   };
 
   // We create two viewers, a 2D and a 3D one
@@ -33,4 +35,132 @@ $(function() {
   // By default, the `camera.changed` event will trigger when the camera has changed by 50%
   // To make it more sensitive, we can bring down this sensitivity
   view3DL.camera.percentageChanged = 0.1;
+
+  // select default options
+  var selectL = document.getElementById('selectL');
+  var selectR = document.getElementById('selectR');
+
+  // if there's 2 or more options in the selector,
+  //   have the left one select the first one
+  //   and have the right one select the second
+  if (selectL.length >= 2) {
+    selectL.value = selectL[0].value;
+    selectR.value = selectR[1].value;
+  }
+
+  //initialise WMS provider
+  const WMSparameters = {
+    version: '1.3.0',
+    format: 'image/png',
+    srs: 'EPSG:4326',
+    transparent: true,
+    // "exceptions": 'application/vnd.ogc.se_inimage',
+  };
+
+  // TODO: make a selector for the background raster
+  //       currently 1_index_Combined
+
+  var elevationL = new Cesium.WebMapServiceImageryProvider({
+    url : `${gs_endpoint}/wms`,
+    layers : `green:${selectL.value}_1_index_Combined`,
+    parameters: WMSparameters,
+  });
+  var boundaryL = new Cesium.WebMapServiceImageryProvider({
+    url : `${gs_endpoint}/wms`,
+    layers : `green:${selectL.value}_model_boundary`,
+    parameters: WMSparameters,
+  });
+  var streamsL = new Cesium.WebMapServiceImageryProvider({
+    url : `${gs_endpoint}/wms`,
+    layers : `green:${selectL.value}_stream_links`,
+    parameters: WMSparameters,
+  });
+
+  var elevationR = new Cesium.WebMapServiceImageryProvider({
+    url : `${gs_endpoint}/wms`,
+    layers : `green:${selectR.value}_1_index_Combined`,
+    parameters: WMSparameters,
+  });
+  var boundaryR = new Cesium.WebMapServiceImageryProvider({
+    url : `${gs_endpoint}/wms`,
+    layers : `green:${selectR.value}_model_boundary`,
+    parameters: WMSparameters,
+  });
+  var streamsR = new Cesium.WebMapServiceImageryProvider({
+    url : `${gs_endpoint}/wms`,
+    layers : `green:${selectR.value}_stream_links`,
+    parameters: WMSparameters,
+  });
+
+  view3DL.imageryLayers.addImageryProvider(elevationL);
+  view3DL.imageryLayers.addImageryProvider(boundaryL);
+  view3DL.imageryLayers.addImageryProvider(streamsL);
+
+  view3DR.imageryLayers.addImageryProvider(elevationR);
+  view3DR.imageryLayers.addImageryProvider(boundaryR);
+  view3DR.imageryLayers.addImageryProvider(streamsR);
+
+  selectL.addEventListener('change', function() {
+    let layers = [];
+    for (let i = 1; i < view3DL.imageryLayers.length; i++) {
+      layers.push(view3DL.imageryLayers.get(i))
+    }
+    for (let i = 0; i < layers.length; i++) {
+      view3DL.imageryLayers.remove(layers[i])
+    }
+
+    elevationL = new Cesium.WebMapServiceImageryProvider({
+      url : `${gs_endpoint}/wms`,
+      layers : `green:${selectL.value}_1_index_Combined`,
+      parameters: WMSparameters,
+    });
+    boundaryL = new Cesium.WebMapServiceImageryProvider({
+      url : `${gs_endpoint}/wms`,
+      layers : `green:${selectL.value}_model_boundary`,
+      parameters: WMSparameters,
+    });
+    streamsL = new Cesium.WebMapServiceImageryProvider({
+      url : `${gs_endpoint}/wms`,
+      layers : `green:${selectL.value}_stream_links`,
+      parameters: WMSparameters,
+    });
+
+    view3DL.imageryLayers.addImageryProvider(elevationL);
+    view3DL.imageryLayers.addImageryProvider(boundaryL);
+    view3DL.imageryLayers.addImageryProvider(streamsL);
+  })
+
+  selectR.addEventListener('change', function() {
+    let layers = [];
+    for (let i = 1; i < view3DR.imageryLayers.length; i++) {
+      layers.push(view3DR.imageryLayers.get(i))
+    }
+    for (let i = 0; i < layers.length; i++) {
+      view3DR.imageryLayers.remove(layers[i])
+    }
+
+    console.log(view3DR.imageryLayers);
+
+    elevationR = new Cesium.WebMapServiceImageryProvider({
+      url : `${gs_endpoint}/wms`,
+      layers : `green:${selectR.value}_1_index_Combined`,
+      parameters: WMSparameters,
+    });
+    boundaryR = new Cesium.WebMapServiceImageryProvider({
+      url : `${gs_endpoint}/wms`,
+      layers : `green:${selectR.value}_model_boundary`,
+      parameters: WMSparameters,
+    });
+    streamsR = new Cesium.WebMapServiceImageryProvider({
+      url : `${gs_endpoint}/wms`,
+      layers : `green:${selectR.value}_stream_links`,
+      parameters: WMSparameters,
+    });
+
+    view3DR.imageryLayers.addImageryProvider(elevationR);
+    view3DR.imageryLayers.addImageryProvider(boundaryR);
+    view3DR.imageryLayers.addImageryProvider(streamsR);
+  })
+
+
 });
